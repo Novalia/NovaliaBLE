e876272a8537d6055fe3fb2063effcf6cfe16b14
Andrew Sage <andrew@asae.co.uk>
2015-09-02 19:12:10 +0100

    Initial Commit

---
+++ Example/Pods/Target Support Files/NovaliaBLE/NovaliaBLE-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NovaliaBLE : NSObject
+@end
+@implementation PodsDummy_NovaliaBLE
+@end
---
+++ Example/Pods/Target Support Files/Pods-NovaliaBLE_Example/Pods-NovaliaBLE_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_NovaliaBLE_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NovaliaBLE_ExampleVersionString[];
+
